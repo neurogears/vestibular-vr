80d4a084506d204cf08f2207b53924c6aaf5fbfb
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2023-01-04 12:23:34 +0000

    Raised firmware version to 1.0.

--- Firmware/VestibularVrH1/app.c
+++ Firmware/VestibularVrH1/app.c
@@ -29,8 +29,8 @@ void hwbp_app_initialize(void)
     /* Define versions */
     uint8_t hwH = 1;
     uint8_t hwL = 0;
-    uint8_t fwH = 0;
-    uint8_t fwL = 1;
+    uint8_t fwH = 1;
+    uint8_t fwL = 0;
     uint8_t ass = 0;
     
    	/* Start core */
