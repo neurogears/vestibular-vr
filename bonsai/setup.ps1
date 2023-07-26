a578a498eb97284e19a27b850fab9e52bb104311
RoboDoig <erskine94@gmail.com>
2023-07-26 09:45:06 +0100

    Update to Bonsai 2.7.2

--- bonsai/setup.ps1
+++ bonsai/setup.ps1
@@ -1,6 +1,6 @@
 if (!(Test-Path "./Bonsai.exe")) {
     # & dotnet build ../src/VestibularVR.sln --configuration Release
-    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.7/Bonsai.zip" -OutFile "temp.zip"
+    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.7.2/Bonsai.zip" -OutFile "temp.zip"
     Move-Item -Path "NuGet.config" "temp.config"
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Move-Item -Path "temp.config" "NuGet.config" -Force
