41fe6aac223745774b1f44051a4ed1b03c6cb982
RoboDoig <erskine94@gmail.com>
2023-11-30 11:12:38 +0000

    Updated round trip analysis script for updated data

--- Python/vestibular-vr/analysis/round_trip.py
+++ Python/vestibular-vr/analysis/round_trip.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 
 # load raw data
+# data_path_1 = 'C:/VestibularVRData/benchmarks/RoundTrip/Data2023_09_21_18_07_32/PhotoDiode1.bin'
+# data_path_2 = 'C:/VestibularVRData/benchmarks/RoundTrip/Data2023_09_21_18_07_32/PhotoDiode2.bin'
+data_path_1 = 'C:/VestibularVRData/benchmarks/RoundTrip_MatroxOnly/Data2023_11_30_11_40_25/PhotoDiode1.bin'
+data_path_2 = 'C:/VestibularVRData/benchmarks/RoundTrip_MatroxOnly/Data2023_11_30_11_40_25/PhotoDiode2.bin'
 fs = 100000.0
-data_path_1 = 'C:/VestibularVRData/benchmarks/RoundTrip/Data2023_09_21_18_07_32/PhotoDiode1.bin'
-data_path_2 = 'C:/VestibularVRData/benchmarks/RoundTrip/Data2023_09_21_18_07_32/PhotoDiode2.bin'
-window = 100000000
+window = 7000000
 
 with open(data_path_1, 'rb') as f:
     photo_diode_1 = np.fromfile(f, dtype=np.int16).astype(np.single)[0:window]
@@ -26,8 +28,8 @@ photo_diode_2[np.where(photo_diode_2 <= pd2_thresh)] = 0
 photo_diode_2[np.where(photo_diode_2 > pd2_thresh)] = 1
 
 # get peaks
-pd1_peaks, pd1_props = signal.find_peaks(photo_diode_1, prominence=1, width=7000)
-pd2_peaks, pd2_props = signal.find_peaks(photo_diode_2, prominence=1, width=7000)
+pd1_peaks, pd1_props = signal.find_peaks(photo_diode_1, prominence=1, width=1000)
+pd2_peaks, pd2_props = signal.find_peaks(photo_diode_2, prominence=1, width=1000)
 
 pd1_peaks = pd1_props['left_bases']
 pd2_peaks = pd2_props['left_bases']
@@ -38,13 +40,13 @@ pd2_diffs = np.diff(pd2_peaks) / fs
 plt.figure()
 plt.plot(photo_diode_1, 'b')
 plt.plot(pd1_peaks, (photo_diode_1[pd1_peaks]*0)+1, 'bx')
-plt.plot(photo_diode_2, 'b')
-plt.plot(pd2_peaks, (photo_diode_2[pd2_peaks]*0)+1, 'bx')
+plt.plot(photo_diode_2, 'r')
+plt.plot(pd2_peaks, (photo_diode_2[pd2_peaks]*0)+1, 'rx')
 
 plt.figure()
-plt.hist(pd1_diffs / 2)
+plt.hist(pd2_diffs / 2)
 plt.xlabel('round trip time (s)')
 plt.ylabel('frequency')
-plt.title('mean={mean:.5f}, sd={sd:.5f}'.format(mean=np.mean(pd1_diffs / 2), sd=np.std(pd1_diffs / 2)))
+plt.title('mean={mean:.5f}, sd={sd:.5f}'.format(mean=np.mean(pd2_diffs / 2), sd=np.std(pd2_diffs / 2)))
 
 plt.show()
