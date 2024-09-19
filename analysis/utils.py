898c1cc68c7b804ad263f8b920d971f67d37adba
RoboDoig <erskine94@gmail.com>
2024-09-19 14:41:13 +0100

    General harp, experiment event loading

--- analysis/utils.py
+++ analysis/utils.py
@@ -12,7 +12,7 @@ import aeon.io.api as api
 class SessionData(Reader):
     """Extracts metadata information from a settings .jsonl file."""
 
-    def __init__(self, pattern="Metadata"):
+    def __init__(self, pattern):
         super().__init__(pattern, columns=["metadata"], extension="jsonl")
 
     def read(self, file):
@@ -28,17 +28,55 @@ class SessionData(Reader):
         return pd.DataFrame(data, index=timestamps, columns=self.columns)
     
 
+class TimestampedCsvReader(Csv):
+    def __init__(self, pattern, columns):
+        super().__init__(pattern, columns, extension="csv")
+        self._rawcolumns = ["Time"] + columns
+
+    def read(self, file):
+        data = pd.read_csv(file, header=0, names=self._rawcolumns)
+        data["Time"] = data["Time"].transform(lambda x: api.aeon(x))
+        data.set_index("Time", inplace=True)
+        return data
+    
+
+class Video(Csv):
+    def __init__(self, pattern):
+        super().__init__(pattern, columns = ["HardwareCounter", "HardwareTimestamp", "FrameIndex", "Path", "Epoch"], extension="csv")
+        self._rawcolumns = ["Time"] + self.columns[0:2]
+
+    def read(self, file):
+        data = pd.read_csv(file, header=0, names=self._rawcolumns)
+        data["FrameIndex"] = data.index
+        data["Path"] = os.path.splitext(file)[0] + ".avi"
+        data["Epoch"] = file.parts[-3]
+        data["Time"] = data["Time"].transform(lambda x: api.aeon(x))
+        data.set_index("Time", inplace=True)
+        return data
+
+
+class OnixAnalogData(Reader):
+    def __init__(self, pattern="OnixAnalogData"):
+        super().__init__(pattern, columns=["metadata"], extension="bin")
+    
+
+def load(reader: Reader, root: Path) -> pd.DataFrame:
+    root = Path(root)
+    pattern = f"{root.joinpath(reader.pattern).joinpath(reader.pattern)}_*.{reader.extension}"
+    data = [reader.read(Path(file)) for file in glob(pattern)]
+    return pd.concat(data)
+
 def load_json(reader: SessionData, root: Path) -> pd.DataFrame:
     root = Path(root)
-    pattern = f"{root.joinpath(root.name)}_*.jsonl"
+    pattern = f"{root.joinpath(reader.pattern).joinpath(reader.pattern)}_*.{reader.extension}"
     data = [reader.read(Path(file)) for file in glob(pattern)]
     return pd.concat(data)
 
 def load_csv(reader: Csv, root: Path) -> pd.DataFrame:
     root = Path(root)
-    pattern = f"{root.joinpath(root.name)}_*.csv"
-    data = [reader.read(Path(file)) for file in glob(pattern)]
-    return pd.concat(data)
+    pattern = f"{root.joinpath(reader.pattern).joinpath(reader.pattern)}_*.{reader.extension}"
+    data = pd.concat([reader.read(Path(file)) for file in glob(pattern)])
+    return data
 
 def load_harp(reader: Harp, root: Path) -> pd.DataFrame:
     root = Path(root)
